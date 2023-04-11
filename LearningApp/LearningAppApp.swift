536d709fb4929ae130ebcaaff473e545c934492b
tienbui <tienbui1508@gmail.com>
2023-04-11 20:10:05 +0930

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Tien Bui on 11/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
